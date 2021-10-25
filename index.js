262f82d0e0880a9a377ae174e35add343fde06b6
rasel <russelroy15@gmail.com>
2021-10-25 19:35:52 +0600

    WIP on Genius car mechanic server

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
-
+const cors = require("cors");
 const app = express();
 const port = 5000;
 
 // middleware
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rzfie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
