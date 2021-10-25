cf34bbd0b3c8e834239e1d40f13817ae8995215e
rasel <russelroy15@gmail.com>
2021-10-25 18:33:03 +0600

    WIP on Genius car mechanic server

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Running Genius server");
 });
