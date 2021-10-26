5524a79b130320a60b5ee57aaaddf868cd9709af
rasel <russelroy15@gmail.com>
2021-10-27 04:25:58 +0600

    WIP on genius car mechanic server project

--- index.js
+++ index.js
@@ -75,4 +75,15 @@ one time:
 2. Heroku software install
 
 
+Every project
+1. git init
+2. .gitignore (node_module, .env)
+3. push everything to git
+4. make sure you have this script:  "start": "node index.js",
+5. make sure: put process.env.PORT in front of your port number
+6. heroku login
+7. heroku create (only one time for a project)
+8. command: git push heroku main
+
+
 */
